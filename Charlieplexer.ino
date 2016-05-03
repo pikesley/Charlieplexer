ea8c84d9e66e56a3d31046f2896512deaeaae2d6
pikesley <github@orgraphone.org>
2016-05-03 22:38:35 +0100

    12 lights!

--- Charlieplexer.ino
+++ Charlieplexer.ino
@@ -1,9 +1,10 @@
-#define LED_A 5
-#define LED_B 6
-#define LED_C 7
+#define LED_A 4
+#define LED_B 5
+#define LED_C 6
+#define LED_D 7
 #define SLEEP 50
-#define CHASE_WIDTH 4
-#define LIGHTS 6
+#define CHASE_WIDTH 5
+#define LIGHTS 15
 
 // 0 - simple
 // 1 - bounce
@@ -11,10 +12,11 @@
 #define PATTERN 2
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(LED_A, INPUT);
   pinMode(LED_B, INPUT);
   pinMode(LED_C, INPUT);
+  pinMode(LED_D, INPUT);
 }
 
 void loop() {
@@ -50,20 +52,22 @@ void light_some(int start, int width) {
 }
 
 void simple() {
-  for (int i = 1; i <= 6; i++)
+  for (int i = 1; i <= LIGHTS; i++)
   {
     light_led(i);
     delay(SLEEP); 
   }
+
+  Serial.println("------");
 }
 
 void bounce() {
-  for (int i = 1; i <= 6; i++) {
+  for (int i = 1; i <= LIGHTS; i++) {
     light_led(i);
     delay(SLEEP); 
   }
 
-  for (int i = 6; i >= 1; i--) {
+  for (int i = LIGHTS; i >= 1; i--) {
     light_led(i);
     delay(SLEEP);
   }
@@ -73,9 +77,11 @@ void reset_pins() {
   pinMode(LED_A, INPUT);
   pinMode(LED_B, INPUT);
   pinMode(LED_C, INPUT);
+  pinMode(LED_D, INPUT);
   digitalWrite(LED_A, LOW);
   digitalWrite(LED_B, LOW);
   digitalWrite(LED_C, LOW);
+  digitalWrite(LED_D, LOW);
 }
 
 void set_pins(int high_pin, int low_pin) {
@@ -86,8 +92,15 @@ void set_pins(int high_pin, int low_pin) {
   pinMode(low_pin, OUTPUT);
 
   // set logic as required
+  //Serial.print(high_pin);
+  //Serial.print(" HIGH, ");
   digitalWrite(high_pin, HIGH);
+
+  //Serial.print(low_pin);
+  //Serial.println(" LOW");
   digitalWrite(low_pin, LOW);
+
+  //Serial.println("");
 }
 
 void light_led(int led_num) {
@@ -98,17 +111,41 @@ void light_led(int led_num) {
   case 2:
     set_pins(LED_A, LED_B);
     break;
+    
   case 3:
     set_pins(LED_C, LED_B);
     break;
   case 4:
     set_pins(LED_B, LED_C);
     break;
+    
   case 5:
     set_pins(LED_C, LED_A);
     break;
   case 6:
     set_pins(LED_A, LED_C); 
+    break;
+    
+  case 7:
+    set_pins(LED_D, LED_A); 
+    break;  
+  case 8:
+    set_pins(LED_A, LED_D); 
+    break;
+      
+  case 9:
+    set_pins(LED_D, LED_B); 
+    break;
+  case 10:
+    set_pins(LED_B, LED_D); 
+    break;
+
+  case 11:
+    set_pins(LED_D, LED_C); 
+    break;
+
+  case 12:
+    set_pins(LED_C, LED_D); 
     break;
   }
 }
